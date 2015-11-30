e4ca07d9e6bf460ac1b234f6b7f5c036c1ff4c87
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-11-29 20:19:07 -0700

    Start proofs
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

---
+++ build.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+${MATLABROOT?"Please set your MATLABROOT"}/bin/matlab -nojvm -nosplash < hw3.m
+pdflatex --output-directory tex_output/ hw3.tex
+bibtex tex_output/hw3
+pdflatex --output-directory tex_output/ hw3.tex
+pdflatex --output-directory tex_output/ hw3.tex
+
+open -a preview tex_output/hw3.pdf
